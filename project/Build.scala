827bd98b4f021167fea665d1784c01c0c56d1298
juozas <juozas@largecode.com>
2012-12-17 11:36:45 +0200

    updated app for play_rc1

--- project/Build.scala
+++ project/Build.scala
@@ -1,20 +1,25 @@
 import sbt._
 import Keys._
-import PlayProject._
+import play.Project._
 
 object ApplicationBuild extends Build {
 
-    val appName         = "file-upload"
-    val appVersion      = "1.0-SNAPSHOT"
+  val appName         = "quick_test"
+  val appVersion      = "1.0-SNAPSHOT"
 
-    val appDependencies = Seq(
-      "commons-io" % "commons-io" % "2.0.1",
+  val appDependencies = Seq(
+    // Add your project dependencies here,
+    javaCore,
+    javaJdbc,
+    javaEbean,
+    "commons-io" % "commons-io" % "2.0.1",
 
-      "commons-codec" % "commons-codec" % "1.7"
-    )
+    "commons-codec" % "commons-codec" % "1.7"
+  )
 
-    val main = PlayProject(appName, appVersion, appDependencies, mainLang = JAVA).settings(
-      // Add your own project settings here      
-    )
+  val main = play.Project(appName, appVersion, appDependencies).settings(
+    // Add your own project settings here
+  )
 
 }
+
